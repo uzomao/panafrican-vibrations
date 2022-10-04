77f49c7df06e72e62e2a77832d30e75a6fbd5b9f
uzoma <chidumaga@gmail.com>
2022-10-04 19:57:36 +0100

    adds link to spotify playlist

--- src/App.js
+++ src/App.js
@@ -9,11 +9,16 @@ const App = () => {
   return (
     <>
       <header className="app-header">
-        <span role="img" aria-label='globe showing europe-africa'>🌍</span>
-        <h3>Pan-African Vibrations</h3>
-        <p role="button" onClick={() => setShowAboutModal(!showAboutModal)}>
-          {showAboutModal ? <>Home</> : <>About</> }
-        </p>
+        <span role="img" aria-label='globe showing europe-africa' className="header-item">🌍</span>
+        <h3 className="header-item">Pan-African Vibrations</h3>
+        <div className='app-header-right header-item'>
+          <p role="button" onClick={() => setShowAboutModal(!showAboutModal)}>
+            {showAboutModal ? <>Home</> : <>About</> }
+          </p>
+          <a href="https://open.spotify.com/playlist/5JP51wcYiXhLbYthWbbbWZ?si=d928042a27974006" target="_blank" rel="noopener noreferrer">
+            Spotify playlist
+          </a>
+        </div>
       </header>
       <Home showAboutModal={showAboutModal} />
     </>
